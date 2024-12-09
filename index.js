3071d3fad7a25daf46015ff94f9183ab17e0b48f
Kohmmz <jeremiahkoome1@gmail.com>
2024-12-09 21:34:06 +0300

    assignment

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Your code here
+
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+  });
+
+
+  function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+
+  document.addEventListener('keydown',function(e){
+    if (e.key=== 'arrowRight') {
+        moveDodgerRight();
+    }
+  })
+
+  function moveDodgerRight() {
+    var leftNumbers = dodger.style.left.replace('px', '');
+    var left = parseInt(leftNumbers, 10)
+    if (left < 360) {
+      dodger.style.left = `${left + 1}px`
+    }}
+
+    document.addEventListener('keydown', function(e) {
+        if (e.key === "ArrowRight") {
+          moveDodgerRight()
+        }
+      })
